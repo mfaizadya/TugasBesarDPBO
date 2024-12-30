f6dccfcaf185c621c01e4595d726033c1b3567c6
SummaryRex <139709884+SummaryRex@users.noreply.github.com>
2024-12-30 18:13:41 +0700

    Update Subtitle.java

--- AplikasiStreaming/src/main/java/com/mycompany/aplikasistreaming/streamingoption/Subtitle.java
+++ AplikasiStreaming/src/main/java/com/mycompany/aplikasistreaming/streamingoption/Subtitle.java
@@ -2,16 +2,18 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.mycompany.aplikasistreaming.streamingoption;
+package com.maven.streamingoption;
 
 /**
  *
  * @author fathi
  */
-import com.mycompany.aplikasistreaming.streamingoption.StreamingOption;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.Scanner;
 
-class Subtitle extends StreamingOption {
+public class Subtitle extends StreamingOption {
     private static List<String> subtitleLanguage = new ArrayList<>();
 
     static {
@@ -27,11 +29,26 @@ class Subtitle extends StreamingOption {
         return subtitleLanguage;
     }
 
-    public static String applySubtitle(int pilih) {
-        if (pilih >= 1 && pilih <= subtitleLanguage.size()) {
-            return subtitleLanguage.get(pilih - 1);
-        } else {
-            return "Invalid";
+    public static String applySubtitle() {
+        Scanner scanner = new Scanner(System.in); 
+        boolean running = true;
+        int pilih = -1;
+
+        while (running) {
+            try {
+                System.out.print("Pilih bahasa: ");
+                pilih = scanner.nextInt();
+                if (pilih < 1 || pilih > subtitleLanguage.size()) {
+                    throw new IllegalArgumentException("[Error] Pilih angka 1 sampai " + subtitleLanguage.size() + "!");
+                }
+                running = false;
+            } catch (InputMismatchException e) {
+                System.out.println("[Error] Masukkan angka!");
+                scanner.nextLine();
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
         }
+        return subtitleLanguage.get(pilih - 1);
     }
 }
