5f00bce0dff0f6871270a29cd216516c197439da
SummaryRex <139709884+SummaryRex@users.noreply.github.com>
2024-12-30 18:12:06 +0700

    Update Quality.java

--- AplikasiStreaming/src/main/java/com/mycompany/aplikasistreaming/streamingoption/Quality.java
+++ AplikasiStreaming/src/main/java/com/mycompany/aplikasistreaming/streamingoption/Quality.java
@@ -2,17 +2,20 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.mycompany.aplikasistreaming.streamingoption;
+package com.maven.streamingoption;
 
 /**
  *
  * @author fathi
  */
-import java.util.*;
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.Scanner;
 
-class Quality {
+public class Quality extends StreamingOption{
     private static List<String> resolution = new ArrayList<>();
-
+    
     static {
         resolution.add("480p");
         resolution.add("720p");
@@ -25,11 +28,26 @@ class Quality {
         return resolution;
     }
 
-    public static String applyResolution(int pilih) {
-        if (pilih >= 1 && pilih <= resolution.size()) {
-            return resolution.get(pilih - 1);
-        } else {
-            return "Invalid";
+    public static String applyResolution(){
+        Scanner scanner = new Scanner(System.in); 
+        boolean running = true;
+        int pilih = -1;
+
+        while (running) {
+            try {
+                System.out.print("Pilih resolusi: ");
+                pilih = scanner.nextInt();
+                if (pilih < 1 || pilih > resolution.size()) {
+                    throw new IllegalArgumentException("[Error] Pilih angka 1 sampai " + resolution.size() + "!");
+                }
+                running = false;
+            } catch (InputMismatchException e) {
+                System.out.println("[Error] Masukkan angka!");
+                scanner.nextLine();
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
         }
+        return resolution.get(pilih - 1);
     }
 }
